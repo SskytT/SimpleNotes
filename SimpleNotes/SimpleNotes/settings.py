a348a04d84c60816e9eae72f2fd14968d3dded8a
SskytT <regmail21@yandex.ru>
2024-08-30 20:31:30 +0300

    docker

--- SimpleNotes/SimpleNotes/settings.py
+++ SimpleNotes/SimpleNotes/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g%5$7qyk9^d3#4u&(im_x%qhgk^&buaki4d&sf2r+h&3v7=rv+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
